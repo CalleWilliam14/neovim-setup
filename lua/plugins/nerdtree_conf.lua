15f0c14e239b5ec4d0564e166e0d4e69b40dd417
William Calle Pinto <cwilliam14c@gmail.com>
2024-08-23 19:02:46 -0400

    small configs for nerdtree

--- lua/plugins/nerdtree_conf.lua
+++ lua/plugins/nerdtree_conf.lua
@@ -2,9 +2,13 @@ vim.g.NERDTreeShowHidden = 1
 
 vim.api.nvim_exec([[
   autocmd FileType nerdtree setlocal relativenumber
-  autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+  
+  autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(":quit\<CR>:\<BS>") | endif
+
+  autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(":quit\<CR>:\<BS>") | endif
+
+  autocmd BufWinEnter * if &buftype != 'quickfix' && getcmdwintype() == '' | silent NERDTreeMirror | endif
 ]], false)
 
-vim.keymap.set('n', '<C-b>', ':NERDTreeMirror<CR>:NERDTreeFocus<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-b>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
 
