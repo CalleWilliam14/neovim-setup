8dcde12347c05f1fd2d36df3f8da776626950bb9
William Calle Pinto <cwilliam14c@gmail.com>
2025-07-17 17:59:02 -0400

    blink.cmp setup added

---
+++ lua/plugins/blink_cmp_conf.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        ghost_text = {
+          enabled = false,
+        },
+        menu = {
+          border = "rounded",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+        },
+      },
+    },
+  },
+}
